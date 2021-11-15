6c7f25e2b46cbdc5c83e881613582574c91d9b50
mohamed-amine-guerras <fm_guerras@esi.dz>
2021-11-15 16:41:18 +0100

    feat: initial commit

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ReactStickyNotes from './components/react-sticky-notes';
+import './app.scss';
+function App() {
+    return (
+        <div className="app">
+            <div className="app-header">
+                <h2>Koala Sticky Notes</h2>
+            </div>
+            <div className="app-body">
+                <ReactStickyNotes
+                    backgroundColor="#1F1A24"
+                    useCSS={true}
+                    containerHeight={"95vmin"}
+                />
+            </div>
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+);
